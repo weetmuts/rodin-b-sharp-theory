e5f8cd01634bd8da7adbbfc334570db36a4c3626
Issam Maamria <maamria@users.sourceforge.net>
2010-03-24 14:20:25 +0000

    Bug fixes. Changed plug-in dependencies.
    svn path=/trunk/RuleBasedProver/; revision=8485

--- ac.soton.eventb.ruleBase.theory.core/src/ac/soton/eventb/ruleBase/theory/core/sc/modules/TheoryCategoryModule.java
+++ ac.soton.eventb.ruleBase.theory.core/src/ac/soton/eventb/ruleBase/theory/core/sc/modules/TheoryCategoryModule.java
@@ -20,25 +20,28 @@ import ac.soton.eventb.ruleBase.theory.core.TheoryAttributes;
 import ac.soton.eventb.ruleBase.theory.core.plugin.TheoryPlugin;
 import ac.soton.eventb.ruleBase.theory.core.sc.Messages;
 import ac.soton.eventb.ruleBase.theory.core.sc.TheoryGraphProblem;
-import ac.soton.eventb.ruleBase.theory.core.utils.TheoryUtils;
-import ac.soton.eventb.ruleBase.theory.ui.prefs.facade.PrefsRepresentative;
 
 /**
  * A module for processing theory categories.
  * 
- * <p>The following checks are performed:</p>
- * <ul> 
- *  <li>if a category is specified more than once, then a warning is issued and duplicates are ignored.
- * 	<li>if a category is not pre-defined (in the theory preferences), an error is issued.
+ * <p>
+ * The following checks are performed:
+ * </p>
+ * <ul>
+ * <li>if a category is specified more than once, then a warning is issued and
+ * duplicates are ignored.
+ * <li>if a category is not pre-defined (in the theory preferences), an error is
+ * issued.
  * </ul>
+ * 
  * @author maamria
- *
+ * 
  */
 public class TheoryCategoryModule extends SCProcessorModule {
 
 	public static final IModuleType<TheoryCategoryModule> MODULE_TYPE = SCCore
-		.getModuleType(TheoryPlugin.PLUGIN_ID + ".categoryModule");
-	
+			.getModuleType(TheoryPlugin.PLUGIN_ID + ".categoryModule");
+
 	public void process(IRodinElement element, IInternalElement target,
 			ISCStateRepository repository, IProgressMonitor monitor)
 			throws CoreException {
@@ -46,46 +49,40 @@ public class TheoryCategoryModule extends SCProcessorModule {
 		ITheoryRoot root = (ITheoryRoot) theoryFile.getRoot();
 		ISCTheoryRoot scRoot = (ISCTheoryRoot) target;
 		ICategory[] categories = root.getCategories();
-		if(categories.length != 0){
+		if (categories.length != 0) {
 			monitor.subTask(Messages.bind(Messages.progress_TheoryCategories));
-			ArrayList<String> usedCategories = new ArrayList<String>(); 
-			for(ICategory cat : categories){
+			ArrayList<String> usedCategories = new ArrayList<String>();
+			for (ICategory cat : categories) {
 				// the name of category
 				String catName = cat.getCategory();
 				String elmntName = cat.getElementName();
-				// check first that category is pre-defined
-				if(TheoryUtils.contains(PrefsRepresentative.getCategories(), catName)){
-					// check if the category has already been used
-					if(!usedCategories.contains(catName)){
-						usedCategories.add(catName);
-						createScCategory(catName, elmntName, scRoot, monitor);
-					}
-					else {
-						// issue a warning and ignore cartegory
-						createProblemMarker(cat, TheoryAttributes.CATEGORY_ATTRIBUTE,
-								TheoryGraphProblem.DuplicateCategoryWarning, catName);
-					}
+				// check if the category has already been used
+				if (!usedCategories.contains(catName)) {
+					usedCategories.add(catName);
+					createScCategory(catName, elmntName, scRoot, monitor);
+				} else {
+					// issue a warning and ignore cartegory
+					createProblemMarker(cat,
+							TheoryAttributes.CATEGORY_ATTRIBUTE,
+							TheoryGraphProblem.DuplicateCategoryWarning,
+							catName);
 				}
-				else {
-					// issue an error, only allow pre-defined categories
-					createProblemMarker(cat, TheoryAttributes.CATEGORY_ATTRIBUTE,
-							TheoryGraphProblem.CategoryNotPredefined, catName);
-				}
+
 			}
 		}
 	}
-
 
 	public IModuleType<?> getModuleType() {
 		return MODULE_TYPE;
 	}
 
-	private void createScCategory(String categoryName, String elementName, 
+	private void createScCategory(String categoryName, String elementName,
 			ISCTheoryRoot target, IProgressMonitor monitor)
-	throws RodinDBException{
+			throws RodinDBException {
 		ICategory scCat = target.getCategory(elementName);
 		scCat.create(null, monitor);
-		scCat.setAttributeValue(TheoryAttributes.CATEGORY_ATTRIBUTE,categoryName, monitor);
+		scCat.setAttributeValue(TheoryAttributes.CATEGORY_ATTRIBUTE,
+				categoryName, monitor);
 	}
-	
+
 }
