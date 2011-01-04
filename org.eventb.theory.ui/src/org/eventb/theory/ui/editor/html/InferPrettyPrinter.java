e5fe3434be3ff5861f44ee6b67a1f79a24a63559
Issam Maamria <maamria@users.sourceforge.net>
2011-01-04 22:20:11 +0000

    Theory: small fix to the pretty printer.
    svn path=/trunk/TheoryMathsExtensions/; revision=10502

--- org.eventb.theory.ui/src/org/eventb/theory/ui/editor/html/InferPrettyPrinter.java
+++ org.eventb.theory.ui/src/org/eventb/theory/ui/editor/html/InferPrettyPrinter.java
@@ -48,7 +48,7 @@ public class InferPrettyPrinter extends DefaultPrettyPrinter {
 	@Override
 	public boolean appendSpecialPrefix(IInternalElement parent,
 			String defaultKeyword, IPrettyPrintStream ps, boolean empty) {
-		ps.appendKeyword("____________________");
+		ps.appendKeyword("&nbsp&nbsp&nbsp_________________");
 		return true;
 	}
 
