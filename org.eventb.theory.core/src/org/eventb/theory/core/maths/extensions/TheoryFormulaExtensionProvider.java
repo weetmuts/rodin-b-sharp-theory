6d984758cb2e41c8bd0d83983b987f1c05072bc4
Laurent Voisin <lvoisin@users.sourceforge.net>
2013-06-07 15:53:34 +0000

    Fix dependency on theory path
    The checked theory path file, on which all event-B components depend,
    was not computed appropriately, especially after a clean where the
    sought file does not exist yet.  We now go through the unchecked user
    file.
    
    svn path=/trunk/TheoryMathsExtensions/; revision=15704

--- org.eventb.theory.core/src/org/eventb/theory/core/maths/extensions/TheoryFormulaExtensionProvider.java
+++ org.eventb.theory.core/src/org/eventb/theory/core/maths/extensions/TheoryFormulaExtensionProvider.java
@@ -1,14 +1,20 @@
 /*******************************************************************************
- * Copyright (c) 2011 University of Southampton.
+ * Copyright (c) 2011, 2013 University of Southampton and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Soton - initial API and implementation
+ *     Systerel - dependency on checked theory path
  *******************************************************************************/
 package org.eventb.theory.core.maths.extensions;
 
-import java.util.Collections;
-import java.util.LinkedHashSet;
+import static java.util.Collections.emptySet;
+import static java.util.Collections.singleton;
+import static org.eventb.theory.internal.core.util.CoreUtilities.log;
+
 import java.util.Set;
 
 import org.eventb.core.IEventBRoot;
@@ -16,9 +22,10 @@ import org.eventb.core.ast.FormulaFactory;
 import org.eventb.core.ast.extension.IFormulaExtension;
 import org.eventb.core.extension.IFormulaExtensionProvider;
 import org.eventb.theory.core.ISCTheoryPathRoot;
+import org.eventb.theory.core.ITheoryPathRoot;
 import org.eventb.theory.core.plugin.TheoryPlugin;
-import org.eventb.theory.internal.core.util.CoreUtilities;
 import org.rodinp.core.IRodinFile;
+import org.rodinp.core.IRodinProject;
 import org.rodinp.core.RodinDBException;
 
 /**
@@ -54,28 +61,41 @@ public class TheoryFormulaExtensionProvider implements IFormulaExtensionProvider
 
 	@Override
 	public Set<IRodinFile> getCommonFiles(IEventBRoot root) {
-		try {
-			ISCTheoryPathRoot[] paths = root.getRodinProject().getRootElementsOfType(ISCTheoryPathRoot.ELEMENT_TYPE);
-			if (paths.length == 1){
-				return Collections.singleton(paths[0].getRodinFile());
-			}
-		} catch (RodinDBException e) {
-			CoreUtilities.log(e, "error while getting theory path for project " + root.getRodinProject());
-		}
-		return new LinkedHashSet<IRodinFile>();
+		return emptySet();
 	}
 
 	@Override
 	public Set<IRodinFile> getProjectFiles(IEventBRoot root) {
-		try {
-			ISCTheoryPathRoot[] paths = root.getRodinProject().getRootElementsOfType(ISCTheoryPathRoot.ELEMENT_TYPE);
-			if (paths.length == 1){
-				return Collections.singleton(paths[0].getRodinFile());
-			}
-		} catch (RodinDBException e) {
-			CoreUtilities.log(e, "error while getting theory path for project " + root.getRodinProject());
+		final IRodinProject prj = root.getRodinProject();
+		final ISCTheoryPathRoot pathRoot = findSCTheoryPathRoot(prj);
+		if (pathRoot == null) {
+			return emptySet();
 		}
-		return new LinkedHashSet<IRodinFile>();
+		return singleton(pathRoot.getRodinFile());
+	}
+
+	/**
+	 * Returns the checked theory path of the given project. We do not search
+	 * directly the checked file, as it might not exist yet (e.g., just after a
+	 * project clean), but rather the unchecked file that we convert eventually.
+	 * 
+	 * @param prj
+	 *            some Rodin project
+	 * @return the checked theory path of the given project
+	 */
+	private ISCTheoryPathRoot findSCTheoryPathRoot(IRodinProject prj) {
+		final ITheoryPathRoot[] paths;
+		try {
+			paths = prj.getRootElementsOfType(ITheoryPathRoot.ELEMENT_TYPE);
+		} catch (RodinDBException e) {
+			log(e, "error while getting theory path for project " + prj);
+			return null;
+		}
+		if (paths.length != 1) {
+			log(null, "Several theory paths in project " + prj);
+			return null;
+		}
+		return paths[0].getSCTheoryPathRoot();
 	}
 
 }
