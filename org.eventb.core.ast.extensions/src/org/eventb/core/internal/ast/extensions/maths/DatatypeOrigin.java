9288d8f883a3bbb9ef94fbbed88302a5c503e8c3
Thái Sơn Hoàng <T.S.Hoang@ecs.soton.ac.uk>
2016-05-11 16:02:07 +0100

    - Implement equals() and hashCode() for datatype origins.

--- org.eventb.core.ast.extensions/src/org/eventb/core/internal/ast/extensions/maths/DatatypeOrigin.java
+++ org.eventb.core.ast.extensions/src/org/eventb/core/internal/ast/extensions/maths/DatatypeOrigin.java
@@ -12,8 +12,10 @@
 package org.eventb.core.internal.ast.extensions.maths;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -163,4 +165,174 @@ public class DatatypeOrigin implements IDatatypeOrigin {
 		return t.toArray(new Type[t.size()]);
 	}
 
+	/**
+	 * Equality for datatype origin by comparing the contents.
+	 * 
+	 * @see Object#equals(Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null || this.getClass() != obj.getClass()) {
+			return false;
+		}
+		
+		DatatypeOrigin other = (DatatypeOrigin) obj;
+		
+		// Compare the datatype name.
+		String thisName = this.getName();
+		String otherName = other.getName();
+		if (thisName == null && otherName != null)
+			return false;
+		if (!thisName.equals(otherName))
+			return false;
+		if (!thisName.equals(otherName))
+			return false;
+		
+		// Compare the type arguments.
+		if (!hasSameTypeArguments(this, other)) {
+			return false;
+		}
+		
+		// Compare the constructors.
+		if (!hasSameConstructors(this, other)) {
+			return false;
+		}
+		
+		// Compare the destructors.
+		if (!hasSameDestructors(this, other)) {
+			return false;
+		}
+		
+		// Compare the destructor types.
+		if (!hasSameDestructorTypes(this, other))
+			return false;
+
+		return true;
+	}
+
+	/**
+	 * Utility method for comparing the type arguments.
+	 * 
+	 * @param mine
+	 *            mine datatype origin
+	 * @param other
+	 *            the other's datatype origin
+	 * @return <code>true</code> if the two input origins have the same type
+	 *         arguments including their types.
+	 * @see #equals(Object)
+	 */
+	private boolean hasSameTypeArguments(DatatypeOrigin mine,
+			DatatypeOrigin other) {
+		String[] mineTypeArguments = mine.getTypeArguments();
+		String[] otherTypeArguments = other.getTypeArguments();
+		
+		if (!Arrays.equals(mineTypeArguments, otherTypeArguments)) {
+			return false;
+		}
+		for (String typeArg : mineTypeArguments) {
+			Type mineType = mine.getGivenType(typeArg);
+			Type otherType = other.getGivenType(typeArg);
+			if (mineType == null && otherType != null)
+				return false;
+			if (!mineType.equals(otherType))
+				return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Utility method for comparing the constructors of the datatype origins.
+	 * 
+	 * @param mine
+	 *            mine datatype origin.
+	 * @param other
+	 *            the other's datatype origin.
+	 * @return <code>true</code> if the two input datatype origins have the same
+	 *         SET of constructors.
+	 * @see #equals(Object)
+	 */
+	private boolean hasSameConstructors(DatatypeOrigin mine,
+			DatatypeOrigin other) {
+		Set<String> mineConstructors = new HashSet<String>(Arrays.asList(mine
+				.getConstructors()));
+		Set<String> otherConstructors = new HashSet<String>(Arrays.asList(other
+				.getConstructors()));
+		return mineConstructors.equals(otherConstructors);
+	}
+
+	/**
+	 * Utility method for comparing the destructors of datatype origins.
+	 * 
+	 * @param mine
+	 *            mine datatype origin.
+	 * @param other
+	 *            the other's datatype origin.
+	 * @return <code>true</code> if the two input datatypes have the same arrays
+	 *         of destructors for each constructor.
+	 * @see #equals(Object)
+	 * @see #hasSameConstructors(DatatypeOrigin, DatatypeOrigin)
+	 * @precondition the two origins have the same set of constructors.
+	 */
+	private boolean hasSameDestructors(DatatypeOrigin mine,
+			DatatypeOrigin other) {
+		// Assume the same constructors
+		String[] mineConstructors = mine.getConstructors();
+		for (String constructor : mineConstructors) {
+			String[] mineDestructors = mine.getDestructors(constructor);
+			String[] otherDestructors = other.getDestructors(constructor);
+			if (!Arrays.equals(mineDestructors, otherDestructors))
+				return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Utility method for comparing the destructor types of datatype origins.
+	 * 
+	 * @param mine
+	 *            mine datatype origin.
+	 * @param other
+	 *            the other's datatype origin.
+	 * @return <code>true</code> if the two input datatypes have the same arrays
+	 *         of destructor types for each constructor.
+	 * @see #equals(Object)
+	 * @see #hasSameConstructors(DatatypeOrigin, DatatypeOrigin)
+	 * @precondition the two origins have the same set of constructors.
+	 */
+	private boolean hasSameDestructorTypes(DatatypeOrigin mine,
+			DatatypeOrigin other) {
+		String[] mineConstructors = mine.getConstructors();
+		for (String constructor : mineConstructors) {
+			Type[] mineDestructorTypes = mine.getDestructorTypes(constructor);
+			Type[] otherDestructorTypes = other.getDestructorTypes(constructor);
+			if (!Arrays.equals(mineDestructorTypes, otherDestructorTypes))
+				return false;
+		}
+		return true;
+	}
+
+	/**
+	 * The hash code is computed from the datatype origin's type arguments and
+	 * constructors.
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 19;
+		int result = this.getName().hashCode();
+		String[] typeArgs = this.getTypeArguments();
+		for (String typeArg : typeArgs) {
+			result = prime*result + typeArg.hashCode();
+		}
+		result = prime*result;
+		String[] constrs = this.getConstructors();
+		for (String constr : constrs) {
+			result += constr.hashCode();
+		}
+		result = prime * result + Arrays.hashCode(constrs);
+		return result;
+	}
+
+	
 }
