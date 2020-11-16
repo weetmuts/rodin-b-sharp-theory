df68893eb5f1e9d2127a0e277b2707d8e600e01c
Guillaume Verdier <gverdier@users.sourceforge.net>
2020-11-16 11:41:16 +0100

    Fix bug in dependencies graph
    The vertices map contains the elements themselves and not their
    component name.

--- org.eventb.theory.core/src/org/eventb/theory/core/maths/extensions/dependencies/DependenciesGraph.java
+++ org.eventb.theory.core/src/org/eventb/theory/core/maths/extensions/dependencies/DependenciesGraph.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2013 University of Southampton and others.
+ * Copyright (c) 2010, 2020 University of Southampton and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -104,7 +104,7 @@ public abstract class DependenciesGraph<E extends IEventBRoot> implements
 
 	@Override
 	public boolean contains(E element) {
-		return verticesMap.containsKey(element.getComponentName());
+		return verticesMap.containsKey(element);
 	}
 
 }
