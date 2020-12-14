2eb758986393820c61f18ce0bb8e7786f990bed5
Guillaume Verdier <gverdier@users.sourceforge.net>
2020-12-14 15:11:22 +0100

    UI: fix theory path creation wizard initialization
    The init() method of the wizard was not called, which prevented it from
    selecting the current project by default and forced users to always
    manually select the project.

--- org.eventb.theory.ui/src/org/eventb/theory/language/ui/explorer/actions/CreateTheoryPathActionDelegate.java
+++ org.eventb.theory.ui/src/org/eventb/theory/language/ui/explorer/actions/CreateTheoryPathActionDelegate.java
@@ -1,14 +1,21 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2012, 2020 University of Southampton and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
 package org.eventb.theory.language.ui.explorer.actions;
 
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.custom.BusyIndicator;
 import org.eclipse.ui.IViewActionDelegate;
 import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.PlatformUI;
 import org.eventb.theory.language.ui.wizard.NewTheoryPathWizard;
 
 /**
@@ -29,6 +36,11 @@ public class CreateTheoryPathActionDelegate implements IViewActionDelegate {
 		BusyIndicator.showWhile(view.getViewSite().getShell().getDisplay(), new Runnable() {
 			public void run() {
 				NewTheoryPathWizard wizard = new NewTheoryPathWizard();
+				if (selection instanceof IStructuredSelection) {
+					wizard.init(PlatformUI.getWorkbench(), (IStructuredSelection) selection);
+				} else {
+					wizard.init(PlatformUI.getWorkbench(), StructuredSelection.EMPTY);
+				}
 				WizardDialog dialog = new WizardDialog(view.getViewSite().getShell(), wizard);
 				dialog.setTitle(wizard.getWindowTitle());
 				dialog.open();
