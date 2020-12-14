0ae40f788931d232ac1b14b640fbf63dc712539a
Guillaume Verdier <gverdier@users.sourceforge.net>
2020-12-14 15:50:38 +0100

    UI: replace action delegate with command handler for TheoryPath wizard
    Action delegates are deprecated.

---
+++ org.eventb.theory.ui/src/org/eventb/theory/ui/explorer/handlers/NewTheoryPathHandler.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * Copyright (c) 2020 CentraleSupélec.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+package org.eventb.theory.ui.explorer.handlers;
+
+import static org.eclipse.ui.PlatformUI.getWorkbench;
+import static org.eclipse.ui.handlers.HandlerUtil.getActiveShell;
+import static org.eclipse.ui.handlers.HandlerUtil.getCurrentSelection;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.custom.BusyIndicator;
+import org.eclipse.swt.widgets.Shell;
+import org.eventb.theory.language.ui.wizard.NewTheoryPathWizard;
+
+/**
+ * Command handler to build a wizard for creating a TheoryPath file.
+ *
+ * @author Guillaume Verdier
+ */
+public class NewTheoryPathHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		final Shell shell = getActiveShell(event);
+		if (shell == null) {
+			return null;
+		}
+		ISelection selection = getCurrentSelection(event);
+		final NewTheoryPathWizard wizard = new NewTheoryPathWizard();
+		if (selection instanceof IStructuredSelection) {
+			wizard.init(getWorkbench(), (IStructuredSelection) selection);
+		} else {
+			wizard.init(getWorkbench(), StructuredSelection.EMPTY);
+		}
+		BusyIndicator.showWhile(shell.getDisplay(), new Runnable() {
+			@Override
+			public void run() {
+				final WizardDialog dialog = new WizardDialog(shell, wizard);
+				dialog.create();
+				dialog.open();
+			}
+		});
+		return null;
+	}
+
+}
